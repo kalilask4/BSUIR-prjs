abc1df2bb71cbcb68f4a9465da97d38f0d1795b5
kalilask4 <kalilask4@gmail.com>
2021-10-27 22:24:16 +0300

    add project template for server using ATL

---
+++ ATL_automation_server/ATL-autoSrv/ATL-autoSrv/dllmain.cpp
@@ -0,0 +1,16 @@
+﻿// dllmain.cpp: реализация DllMain.
+
+#include "pch.h"
+#include "framework.h"
+#include "resource.h"
+#include "ATLautoSrv_i.h"
+#include "dllmain.h"
+
+CATLautoSrvModule _AtlModule;
+
+// Точка входа DLL
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	hInstance;
+	return _AtlModule.DllMain(dwReason, lpReserved);
+}
---
+++ ATL_automation_server/ATL-autoSrv/ATL-autoSrv/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: объявление класса модуля.
+
+class CATLautoSrvModule : public ATL::CAtlDllModuleT< CATLautoSrvModule >
+{
+public :
+	DECLARE_LIBID(LIBID_ATLautoSrvLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_ATLAUTOSRV, "{6d2ec9b2-2f7a-4358-8fb4-d333b8edd6fa}")
+};
+
+extern class CATLautoSrvModule _AtlModule;
