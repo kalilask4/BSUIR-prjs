d42a1b9a0195d2966c670c2c374e49512ee035a3
kalilask4 <kalilask4@gmail.com>
2021-12-25 19:01:06 +0300

    Add c# client

---
+++ CalClientSolSR/CalClientSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿//Program.cs
+using System;
+using MSACAL;
+
+namespace CalClientSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SR3 00321 kotsubad");
+            var obj = new Calendar();
+
+            Console.WriteLine("Текущий год " + obj.Year);
+            Console.WriteLine("Текущий месяц " + obj.Month);
+            
+
+            obj.Month = 6;
+            obj.Year = 1997;
+
+            Console.WriteLine("Обновленный год " + obj.Year);
+            Console.WriteLine("Обновленный месяц " + obj.Month);
+
+            Console.ReadKey();
+        }
+    }
+}
